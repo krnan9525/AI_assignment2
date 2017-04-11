df880c1b4f925de750416d519dae3eb55a30e35c
krnan9525 <sepyng@gmail.com>
2017-04-11 23:20:37 +0100

    ID3_1ST

--- ID3_approach.py
+++ ID3_approach.py
@@ -1,3 +1,20 @@
-from ete2 import Tree
-t= Tree()
-t.populate(50)
+import queue
+
+from treelib import Node, Tree
+
+
+current_leaves = queue.PriorityQueue()
+class NodeData(object):
+        def __init__(self, name, reminder):
+            self.name = name
+            self.reminder = reminder
+
+        def __cmp__(self, other):
+            return 1
+
+current_leaves.put(NodeData(1,1))
+print(current_leaves)
+tree = Tree()
+current_node = tree.create_node("hi",0,data = NodeData("hi","hkljsdf"))
+print(current_node.data.reminder)
+tree.show()
